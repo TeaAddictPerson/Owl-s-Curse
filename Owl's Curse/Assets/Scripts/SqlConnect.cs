4420c62e2c01a221ea6506571a1b8cbc5de2c901
TeaAddictPerson <maksimovna2006@gmail.com>
2024-11-26 23:31:53 +0300

    Sql connect & start screen

---
+++ Owl's Curse/Assets/Scripts/SqlConnect.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Data;
+using System.IO;
+
+public class SqlConnect : MonoBehaviour
+{
+    void Start()
+    {
+        CheckDatabaseConnection();
+    }
+
+    void CheckDatabaseConnection()
+    {
+      
+        string dbPath = Path.Combine(Application.dataPath, "OwlsCurse.db");
+
+  
+        string connectionString = $"URI=file:{dbPath}";
+
+        try
+        {
+            
+            using (IDbConnection dbConnection = new SqliteConnection(connectionString))
+            {
+                dbConnection.Open(); 
+
+               
+                if (dbConnection.State == ConnectionState.Open)
+                {
+                    Debug.Log("Подключение к базе данных успешно!");
+                }
+                else
+                {
+                    Debug.LogWarning("Не удалось подключиться к базе данных.");
+                }
+            }
+        }
+        catch (System.Exception ex)
+        {
+            
+            Debug.LogError($"Ошибка при подключении к базе данных: {ex.Message}");
+        }
+    }
+}
