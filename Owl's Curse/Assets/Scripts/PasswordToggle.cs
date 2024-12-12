8dce517fd96403a9018053a5c5d29aaa4231bf6a
TeaAddictPerson <maksimovna2006@gmail.com>
2024-12-12 18:18:31 +0300

    PasswordMask

---
+++ Owl's Curse/Assets/Scripts/PasswordToggle.cs
@@ -0,0 +1,31 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PasswordToggle : MonoBehaviour
+{
+    public TMP_InputField passwordField; 
+    public Image eyeButtonImage;       
+    public Sprite eyeOpenSprite;        
+    public Sprite eyeClosedSprite;      
+
+    private bool isPasswordVisible = false;
+
+    public void TogglePasswordVisibility()
+    {
+        isPasswordVisible = !isPasswordVisible;
+
+        if (isPasswordVisible)
+        {
+            passwordField.contentType = TMP_InputField.ContentType.Standard;
+            eyeButtonImage.sprite = eyeOpenSprite; 
+        }
+        else
+        {
+
+            passwordField.contentType = TMP_InputField.ContentType.Password;
+            eyeButtonImage.sprite = eyeClosedSprite; 
+        }
+        passwordField.ForceLabelUpdate();
+    }
+}
