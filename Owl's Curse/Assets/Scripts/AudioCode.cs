cb1b6eb54f578a9359035987960625cfef26071d
TeaAddictPerson <maksimovna2006@gmail.com>
2024-12-16 01:07:04 +0300

    Final

---
+++ Owl's Curse/Assets/Scripts/AudioCode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioCode : MonoBehaviour
+{
+    private static AudioCode instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Owl's Curse/Assets/Scripts/AudioController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioController : MonoBehaviour
+{
+    public Sprite audioOn;
+    public Sprite audioOff;
+    public GameObject buttonAudio;
+
+    public AudioClip clip;
+    public AudioSource audio;
+
+    private void Update()
+    {
+
+    }
+
+    public void OnOffAudio()
+    {
+        if(AudioListener.volume==1)
+        {
+            AudioListener.volume = 0;
+            buttonAudio.GetComponent<Image>().sprite=audioOff;
+        }
+        else
+        {
+            AudioListener.volume = 1;
+            buttonAudio.GetComponent<Image>().sprite = audioOn;
+        }
+    }
+}
